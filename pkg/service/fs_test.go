0ce4253f74eed7a8ef1877bf758a8273385a5a0d
cpowicki <chpowicki@gmail.com>
2021-09-12 21:53:30 -0700

    add test for read file contents

--- pkg/service/fs_test.go
+++ pkg/service/fs_test.go
@@ -22,6 +22,43 @@ func TestListDirContents(t *testing.T) {
 	if len(metadata) != 3 {
 		t.Fatalf(`expecting len 3, got %d`, len(metadata))
 	}
+
+	if metadata[0].FileName != "data.txt" {
+		t.Fatalf(`expecting 'data.txt', got %s`, metadata[0].FileName)
+	}
+
+	if metadata[0].Owner != "Unknown" {
+		t.Fatalf(`expecting 'Unknown', got %s`, metadata[0].Owner)
+	}
+
+	if metadata[0].Type != "File" {
+		t.Fatalf(`expecting 'File', got %s`, metadata[0].Type)
+	}
+
+	if metadata[0].Permissions != "777" {
+		t.Fatalf(`expecting '777', got %s`, metadata[0].Permissions)
+	}
+
+	if metadata[1].FileName != "directory" {
+		t.Fatalf(`expecting 'directory', got %s`, metadata[0].FileName)
+	}
+
+	if metadata[1].Type != "Directory" {
+		t.Fatalf(`expecting 'Directory', got %s`, metadata[0].Type)
+	}
+}
+
+func TestReadFileContents(t *testing.T) {
+	var fsService = FileSystemService{
+		root: "/",
+		fs:   mockFileSystem(),
+	}
+
+	contents, _ := fsService.ReadFileContents("data.txt")
+
+	if contents != "this file has contents" {
+		t.Fatalf(`expecting 'this file has contents', got '%s'`, contents)
+	}
 }
 
 func mockFileSystem() (testFs afero.Fs) {
