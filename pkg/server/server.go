f33ec834ae05ba9b727895bc787eb03a058c6e5b
cpowicki <chpowicki@gmail.com>
2021-09-12 20:14:59 -0700

    Add middleware and content reading

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/cpowicki/fs-api/pkg/config"
 	"github.com/cpowicki/fs-api/pkg/service"
@@ -23,13 +24,43 @@ func NewFileSystemServer(config config.FsApiConfig) FileSystemServer {
 
 func (s *FileSystemServer) Start() {
 
-	http.HandleFunc("/", s.listRootDir)
+	http.HandleFunc("/", s.handleRequest)
 
 	http.ListenAndServe(fmt.Sprintf("%s%d", ":", s.port), nil)
+
+}
+
+func (s *FileSystemServer) handleRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(405)
+		return
+	}
+
+	var path = strings.TrimPrefix(r.URL.Path, "/")
+
+	if path == "" {
+		s.listRootDir(w, r)
+	} else {
+		if s.fsService.IsDirectory(path) {
+			s.listDir(path, w, r)
+		} else {
+			s.readFileContents(path, w, r)
+		}
+	}
 
 }
 
 func (s *FileSystemServer) listRootDir(w http.ResponseWriter, r *http.Request) {
 	metdata, _ := s.fsService.ListRootDirContents()
 	json.NewEncoder(w).Encode(metdata)
+}
+
+func (s *FileSystemServer) listDir(dir string, w http.ResponseWriter, r *http.Request) {
+	metdata, _ := s.fsService.ListDirContents(dir)
+	json.NewEncoder(w).Encode(metdata)
+}
+
+func (s *FileSystemServer) readFileContents(path string, w http.ResponseWriter, r *http.Request) {
+	data, _ := s.fsService.ReadFileContents(path)
+	w.Write([]byte(data))
 }
--- pkg/service/fs.go
+++ pkg/service/fs.go
@@ -3,6 +3,7 @@ package service
 import (
 	"fmt"
 	"io/fs"
+	"io/ioutil"
 	"os"
 	"os/user"
 	"path/filepath"
@@ -17,6 +18,7 @@ type FileMetadata struct {
 	Owner       string
 	Size        string
 	Permissions string
+	Type        string
 }
 
 type FileSystemService struct {
@@ -30,10 +32,11 @@ func NewFileSystemService(fsApiConfig config.FsApiConfig) FileSystemService {
 }
 
 func (service *FileSystemService) ListRootDirContents() (metadata []FileMetadata, err error) {
-	return service.ListDirContents(service.root)
+	return service.ListDirContents("")
 }
 
-func (service *FileSystemService) ListDirContents(path string) (metadata []FileMetadata, err error) {
+func (service *FileSystemService) ListDirContents(relativePath string) (metadata []FileMetadata, err error) {
+	var path = filepath.Join(service.root, relativePath)
 
 	fileinfo, err := os.ReadDir(path)
 	if err != nil {
@@ -49,7 +52,6 @@ func (service *FileSystemService) ListDirContents(path string) (metadata []FileM
 }
 
 func (service *FileSystemService) buildFileMetadata(dir string, entry fs.DirEntry) FileMetadata {
-
 	var fullPath = filepath.Join(dir, entry.Name())
 
 	fileInfo, err := os.Stat(fullPath)
@@ -57,11 +59,20 @@ func (service *FileSystemService) buildFileMetadata(dir string, entry fs.DirEntr
 		panic(err)
 	}
 
+	var t string
+
+	if fileInfo.IsDir() {
+		t = "Directory"
+	} else {
+		t = "File"
+	}
+
 	return FileMetadata{
 		FileName:    fileInfo.Name(),
 		Owner:       service.getFileOwner(fileInfo),
-		Size:        "",
+		Size:        fmt.Sprintf("%d bytes", fileInfo.Size()),
 		Permissions: "",
+		Type:        t,
 	}
 }
 
@@ -78,6 +89,24 @@ func (service *FileSystemService) getFileOwner(fileInfo fs.FileInfo) (username s
 	} else {
 		username = usr.Name
 	}
+
+	return
+}
+
+func (service *FileSystemService) IsDirectory(path string) bool {
+	var fullPath = filepath.Join(service.root, path)
+	// TODO handle err
+	fileInfo, _ := os.Stat(fullPath)
+	return fileInfo.IsDir()
+
+}
+
+func (service *FileSystemService) ReadFileContents(file string) (contents string, err error) {
+	var path = filepath.Join(service.root, file)
+
+	bytes, err := ioutil.ReadFile(path)
+
+	contents = string(bytes)
 
 	return
 }
